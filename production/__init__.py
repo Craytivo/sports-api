ea990e126574fcd46c39842004e78f197a43a671
Craytivo <craytivo@gmail.com>
2026-09-15 14:32:21 -0600

    Export team ratings integration

--- production/__init__.py
+++ production/__init__.py
@@ -3,6 +3,15 @@
 from .ingestion import GameIngestion
 from .models import CanonicalGame
 from .provider import SportsProvider
+from .ratings import ESPNPowerIndexRatingsProvider, TeamRating, TeamRatingsProvider
 from .scoring import score_game
 
-__all__ = ["CanonicalGame", "GameIngestion", "SportsProvider", "score_game"]
+__all__ = [
+    "CanonicalGame",
+    "ESPNPowerIndexRatingsProvider",
+    "GameIngestion",
+    "SportsProvider",
+    "TeamRating",
+    "TeamRatingsProvider",
+    "score_game",
+]
